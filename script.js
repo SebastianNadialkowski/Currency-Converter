112f3ba04031586013f2ea27370a1bb2db7fc21d
SebastianNadialkowski <121190741+SebastianNadialkowski@users.noreply.github.com>
2023-01-16 18:10:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 {
     const calculateResult = (amount, currency) => {
-
         const rateEUR = 4.6843;
         const rateUSD = 4.4100;
         const rateGBP = 5.4418;
         const rateCHF = 4.7482;
         const rateJPY = 3.2291;
-
         switch (currency) {
             case "EUR":
                 return amount / rateEUR;
@@ -23,24 +21,18 @@
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-
         const amountElement = document.querySelector(".js-amount");
         const currencyElement = document.querySelector(".js-currency");
         const resultElement = document.querySelector(".js-result");
-
         const amount = +amountElement.value;
         const currency = currencyElement.value
-
         const result = calculateResult(amount, currency);
         resultElement.innerText = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
     }
 
     const init = () => {
-
         const formElement = document.querySelector(".js-form");
-
         formElement.addEventListener("submit", onFormSubmit);
-
     };
 
     init();
